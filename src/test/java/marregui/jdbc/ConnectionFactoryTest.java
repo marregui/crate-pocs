c8f27aa30ae98c80c1e90d24598350f037339e4f
marregui <miguel.arregui@gmail.com>
2020-12-14 11:01:43 +0100

    Add the testing framework

---
+++ src/test/java/marregui/jdbc/ConnectionFactoryTest.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed to Miguel Arregui ("marregui") under one or more contributor
+ * license agreements. See the LICENSE file distributed with this work
+ * for additional information regarding copyright ownership. You may
+ * obtain a copy at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Copyright 2020, Miguel Arregui a.k.a. marregui
+ */
+
+package marregui.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class ConnectionFactoryTest {
+
+
+    @Test
+    public void test_getConnection() {
+        mock(ConnectionFactory.class);
+    }
+}
